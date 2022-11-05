0d98e3e1e4c1f69b1f1f245b8824b32d9ea98959
IrynaVlasenko <106557356+IrynaVlasenko@users.noreply.github.com>
2022-11-05 15:25:44 +0200

    search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Learn React
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by Iryna Vlasenko</footer>
     </div>
   );
 }
