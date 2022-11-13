1de834dfa1f24cd80a439c29a1d7fd9ff69776dd
IrynaVlasenko <106557356+IrynaVlasenko@users.noreply.github.com>
2022-11-13 17:14:24 +0200

    styling2

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,8 +3,9 @@ import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [loaded, setLoaded] = useState(false);
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
@@ -12,27 +13,43 @@ export default function Dictionary() {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          autoFocus={true}
-          onChange={handleKeywordChange}
-          className="Dictionary-input"
-        />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              autoFocus={true}
+              onChange={handleKeywordChange}
+              className="Dictionary-input"
+            />
+          </form>
+        </section>
+        <div className="hint">suggested words: sunset, wine, yoga...</div>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,25 +1,24 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-  return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <strong>Defenition:</strong> {definition.defenition}
-              <br />
-              <strong>Example:</strong>
+  if (props.meaning) {
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
+            <div key={index}>
+              <p>{definition.definition} </p>
               <em>{definition.example}</em>
               <Synonyms synonyms={definition.synonyms} />
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
